c67ee277b12d6b747e07d07246caefd7f61435ef
Gian <lanzierigian1207@gmail.com>
2026-04-19 20:40:06 -0300

    Swap Mendoza form and mail images

--- api/inscripciones.js
+++ api/inscripciones.js
@@ -78,7 +78,7 @@ const MAIL_EVENT_EXTRAS = [
     hideBuenDiaLine: true,
     donationNotice:
       "Ingreso solidario: para participar del evento, es necesario llevar 2 alimentos no perecederos.",
-    imageUrl: "https://plomerosarg.com/Prueba_2/assets/mendoza-mail.jpeg"
+    imageUrl: "https://plomerosarg.com/Prueba_2/assets/mendoza-nos-acompanan.jpeg"
   }
 ];
 const WHATSAPP_GROUP_MATCHERS = [
